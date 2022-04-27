6c62215928a5ed4ae7a3b9295ab189c67da8d22f
Oxana Galyamova <mrs.oxas@gmail.com>
2022-04-27 20:13:04 +0500

    first review fixes

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,33 +1,34 @@
 const popupOpenBtn = document.querySelector('.profile__btn_action_edit');
-const popup = document.querySelector('.form');
+const popup = document.querySelector('.popup');
 const popupCloseBtn = document.querySelector('.form__btn_action_close');
-
-popupOpenBtn.addEventListener('click', function(){
-  togglePopup()
-});
-
-popupCloseBtn.addEventListener('click', function(){
-  togglePopup()
-});
-
-function togglePopup(){
-  popup.classList.toggle('hidden')
-};
-
 
 const formElement = document.querySelector('.form');
 const nameInput = formElement.querySelector('.form__text-name');
 const jobInput = formElement.querySelector('.form__text-info');
 
+const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+
+function togglePopup(){
+
+  if(popup.classList.contains('hidden') === false){
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileDescription.textContent;
+  }
+
+  popup.classList.toggle('hidden')
+};
+
 function formSubmitHandler (evt) {
-    evt.preventDefault();
+  evt.preventDefault();
 
-    let profileTitle = document.querySelector('.profile__title');
-    let profileDescription = document.querySelector('.profile__description');
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
 
-
-    profileTitle.textContent = nameInput.value;
-    profileDescription.textContent = jobInput.value;
+  togglePopup()
 }
+
+popupOpenBtn.addEventListener('click', togglePopup);
+popupCloseBtn.addEventListener('click', togglePopup);
 
 formElement.addEventListener('submit', formSubmitHandler);
